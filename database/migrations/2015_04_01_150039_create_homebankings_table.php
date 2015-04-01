99c746dd3b2ec0d39822997741ed3ece603efb90
miguelrebola21 <miguelrebola21@gmail.com>
2015-04-01 16:41:03 +0100

    nothing

---
+++ database/migrations/2015_04_01_150039_create_homebankings_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateHomebankingsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('homebankings', function(Blueprint $table)
+		{
+				$table->increments('id')->unsigned();
+				$table->integer('id_cliente')->unsigned();
+				$table->foreign('id_cliente')->references('id')->on('clientes');
+				$table->string('email')->unique();
+				$table->foreign('email')->references('email')->on('clientes');
+				$table->string('password');
+				$table->rememberToken();
+				$table->boolean('valido');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('homebankings');
+	}
+
+}
