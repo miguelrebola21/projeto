0458269d87e3c890af006b6887b643ce19471e0f
miguelrebola21 <miguelrebola21@gmail.com>
2015-04-16 16:39:38 +0100

    migrate fix

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -21,16 +21,19 @@ class DatabaseSeeder extends Seeder {
 	 */
 	public function run()
 	{
-		cliente::create(array('nome' => 'Miguel Rebola', 'bi' => '15271173', 'telefone' => '968823502', 'morada' => 'Rua Maria Archer nº11 1ºEsq' , 'cp' => '2650-189' , 'email' => 'miguelrebola21@gmail.com' , 'valido' => '1'));
+		$cli=cliente::create(array('nome' => 'Miguel Rebola', 'bi' => '15271173', 'telefone' => '968823502', 'morada' => 'Rua Maria Archer nº11 1ºEsq' , 'cp' => '2650-189' , 'email' => 'miguelrebola21@gmail.com' , 'valido' => '1'));
+		$id=$cli->id;
 		Roles::create(array('name' => 'Cliente'));
 		Roles::create(array('name' => 'Balcão'));
 		Roles::create(array('name' => 'Escritório'));
 		Roles::create(array('name' => 'Gerente'));
-		Roles::create(array('name' => 'Administrador'));
-		Roles_Clientes::create(array('cliente_id' => '1', 'roles_id' => '5'));
-		conta::create(array('pin' => '1234', 'tipo' => 'Particular', 'agencia' => '1', 'saldo' => '1000'));
-		clientes_conta::create(array('conta_id' => '1', 'cliente_id' => '1'));
-		homebanking::create(array('id_cliente' => '1', 'email' => 'miguelrebola21@gmail.com' , 'password' => '$2y$10$u7/SLIT/S4D4tACruWtvoeJYjNKEJ/QswoV72heNR00m80XLki7LK' , 'valido' => '1'));
+		$rol=Roles::create(array('name' => 'Administrador'));
+		$idrol=$rol->id;
+		Roles_Clientes::create(array('cliente_id' => $id, 'roles_id' => $idrol));
+		$con=conta::create(array('pin' => '1234', 'tipo' => 'Particular', 'agencia' => '1', 'saldo' => '1000'));
+		$idcon=$con->id;
+		clientes_conta::create(array('conta_id' => $idcon, 'cliente_id' => $id));
+		homebanking::create(array('id_cliente' => $id, 'email' => 'miguelrebola21@gmail.com' , 'password' => '$2y$10$u7/SLIT/S4D4tACruWtvoeJYjNKEJ/QswoV72heNR00m80XLki7LK' , 'valido' => '1'));
 
 	}
 
