f0aad5afa9932c458b380c808f172038f876f68d
miguelrebola21 <miguelrebola21@gmail.com>
2015-04-16 12:08:31 +0100

    revert to mysql

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,6 +2,15 @@
 
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
+use App\cliente;
+use App\conta;
+use App\homebanking;
+use App\clientes_conta;
+use App\suporte;
+use App\correio;
+use App\roles;
+use App\clientes_contas;
+use App\Roles_Clientes;
 
 class DatabaseSeeder extends Seeder {
 
@@ -21,6 +30,8 @@ class DatabaseSeeder extends Seeder {
 		Roles_Clientes::create(array('cliente_id' => '1', 'roles_id' => '5'));
 		conta::create(array('pin' => '1234', 'tipo' => 'Particular', 'agencia' => '1', 'saldo' => '1000'));
 		clientes_conta::create(array('conta_id' => '1', 'cliente_id' => '1'));
+		homebanking::create(array('id_cliente' => '1', 'email' => 'miguelrebola21@gmail.com' , 'password' => '$2y$10$u7/SLIT/S4D4tACruWtvoeJYjNKEJ/QswoV72heNR00m80XLki7LK' , 'valido' => '1'));
+
 	}
 
 }
